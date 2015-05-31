1132345aa9aa9c56e10d18a676ff3c241d9d0f37
Beraxx <cermor@web.de>
2015-05-31 19:00:54 +0200

    nochmal

---
+++ EliteEngine/src/entity/entities/Arol.java
@@ -0,0 +1,110 @@
+package entity.entities;
+
+import entity.Attacker;
+import entity.Entity;
+import entity.Unit;
+import entity.animation.Ability;
+import entity.animation.Animation;
+import entity.animation.AreaAttack;
+import entity.animation.Death;
+import processing.core.PImage;
+import shared.Nation;
+
+public class Arol extends Unit implements Attacker {
+
+	private static PImage standingImg;
+
+	byte aggroRange;
+
+	AreaAttack basicAttack;
+
+	public static void loadImages() {
+		String path = path(Nation.ALIENS, new Object() {
+		});
+		standingImg = game.ImageHandler.load(path, "Arol");
+	}
+
+	public Arol(String[] c) {
+		super(c);
+		iconImg = standingImg;
+
+		stand = new Animation(standingImg, 100);
+		walk = new Animation(standingImg, 800);
+		death = new Death(standingImg, 500);
+		basicAttack = new AreaAttack(standingImg, 800);
+
+		animation = nextAnimation = walk;
+		// ************************************
+		xSize = 100;
+		ySize = 70;
+
+		hp = hp_max = 2500;
+		speed = 0.8f;
+		radius = 20;
+		sight = 120;
+		groundPosition = Entity.GroundPosition.GROUND;
+
+		aggroRange = (byte) (radius + 100);
+		basicAttack.range = (byte) (radius + 20);
+		basicAttack.damage = 40;
+		basicAttack.cooldown = 5000;
+		basicAttack.eventTime = 500;
+		// ************************************
+	}
+
+	@Override
+	public void updateDecisions() {
+
+		// isTaged = false;
+		if (animation == stand) {// ****************************************************
+			String s = "";
+			for (Entity e : player.visibleEntities) {
+				if (e != this) {
+					if (e.isEnemyTo(this)) {// server
+						if (e.isCollision(x, y, aggroRange + e.radius)) {
+							s = ("walk " + e.x + " " + e.y);
+						}
+					}
+				}
+			}
+			sendAnimation(s);
+		}
+		if (animation == walk) {// ****************************************************
+			float importance = 0;
+			Entity importantEntity = null;
+			for (Entity e : player.visibleEntities) {
+				if (e != this) {
+					if (e.isEnemyTo(this)) {
+						if (e.isCollision(x, y, basicAttack.range + e.radius)) {
+							// evtl abfrage wegen groundposition
+
+							float newImportance = calcImportanceOf(e);
+							if (newImportance > importance) {
+								importance = newImportance;
+								importantEntity = e;
+							}
+						}
+					}
+				}
+			}
+			if (importantEntity != null && getBasicAttack().isNotOnCooldown()) {
+				// System.out.println(thread);
+				sendAnimation("basicAttack " + x + " " + y);
+			}
+		}
+		basicAttack.updateAbility(this);
+	}
+
+	@Override
+	public void renderGround() {
+		drawSelected();
+		animation.draw(this, direction, currentFrame);
+		drawTaged();
+	}
+
+	@Override
+	public Ability getBasicAttack() {
+		return basicAttack;
+	}
+
+}
